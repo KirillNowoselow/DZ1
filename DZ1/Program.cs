59575ab241dc90633237dc48d82010e147b630cb
user <kirillnowoselow4@gmail.com>
2023-02-02 14:17:38 +0300

    Добавьте файлы проекта.

---
+++ DZ1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 * x");
+            Console.WriteLine("Math.Sin(x)");
+            Console.WriteLine("Math.Pow(a, 2)");
+            Console.WriteLine("Math.Sqrt(x)");
+            Console.WriteLine("Math.Abs(x)");
+            Console.WriteLine("5 * Math.Cos(y)");
+            Console.WriteLine("-7,5 * Math.Pow(a, 2)");
+            Console.WriteLine("3 * Math.Sqrt(x)");
+            Console.WriteLine("Math.Sin(a) * Math.Cos(b) + Math.Cos(a) * Math.Sin(b)");
+            Console.WriteLine("a * Math.Sqrt(2 * b)");
+            Console.WriteLine("3 * Math.Sin(2 * a) * Math.Cos(3 * b)");
+            Console.WriteLine("-5 * Math.Sqrt(x + Math.Sqrt(y))");
+        }
+    }
+}
