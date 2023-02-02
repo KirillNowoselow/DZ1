a00463cf88eb3f252e0faa46e0b2417ebaca5ec3
xa4ipur4ik <98112009+KirillNowoselow@users.noreply.github.com>
2023-02-02 16:20:54 +0300

    Update Program.cs

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -10,18 +10,18 @@ namespace DZ1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("2 * x");
-            Console.WriteLine("Math.Sin(x)");
-            Console.WriteLine("Math.Pow(a, 2)");
-            Console.WriteLine("Math.Sqrt(x)");
-            Console.WriteLine("Math.Abs(x)");
-            Console.WriteLine("5 * Math.Cos(y)");
-            Console.WriteLine("-7,5 * Math.Pow(a, 2)");
-            Console.WriteLine("3 * Math.Sqrt(x)");
-            Console.WriteLine("Math.Sin(a) * Math.Cos(b) + Math.Cos(a) * Math.Sin(b)");
-            Console.WriteLine("a * Math.Sqrt(2 * b)");
-            Console.WriteLine("3 * Math.Sin(2 * a) * Math.Cos(3 * b)");
-            Console.WriteLine("-5 * Math.Sqrt(x + Math.Sqrt(y))");
+            Console.WriteLine("a) 2 * x");
+            Console.WriteLine("б) Math.Sin(x)");
+            Console.WriteLine("в) Math.Pow(a, 2)");
+            Console.WriteLine("г) Math.Sqrt(x)");
+            Console.WriteLine("д) Math.Abs(x)");
+            Console.WriteLine("е) 5 * Math.Cos(y)");
+            Console.WriteLine("ж) -7,5 * Math.Pow(a, 2)");
+            Console.WriteLine("з) 3 * Math.Sqrt(x)");
+            Console.WriteLine("и) Math.Sin(a) * Math.Cos(b) + Math.Cos(a) * Math.Sin(b)");
+            Console.WriteLine("к) a * Math.Sqrt(2 * b)");
+            Console.WriteLine("л) 3 * Math.Sin(2 * a) * Math.Cos(3 * b)");
+            Console.WriteLine("м) -5 * Math.Sqrt(x + Math.Sqrt(y))");
         }
     }
 }
